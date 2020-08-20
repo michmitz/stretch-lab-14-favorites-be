d92bd409be2c1a80950a9df37a187f9184459439
Michelle Stermitz <stermitzm@gmail.com>
2020-08-20 16:13:16 -0700

    Updated create-tables, seed data and load-seed-data

---
+++ data/favorites.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: 'Margarita',
+    image: 'some image',
+  },
+  {
+    name: 'Blue Margarita',
+    image: 'some image'
+  },
+  {
+    name: 'Tommy\'s Margarita',
+    image: 'some image'
+  },
+  {
+    name: 'Whitecap Margarita',
+    image: 'some image'
+  }
+];
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const animals = require('./favorites.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,12 +25,12 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      favorites.map(favorite => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
+                    INSERT INTO favorites (name, image, user_id)
                     VALUES ($1, $2, $3);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+        [favorite.strDrink, favorite.strDrinkThumb, user.id]);
       })
     );
     
